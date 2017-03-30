0fa9520c76198a61839b5fddc0a6fd70fcbfd261
Hamza Tariq <hamzatrq116@gmail.com>
2017-03-31 00:16:50 +0500

    initial commit

---
+++ Controllers/CmsController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace cms.Controllers
+{
+    public class CmsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Doctors()
+        {
+            return View();
+        }
+
+        public IActionResult Faqs()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+        public IActionResult Services()
+        {
+            return View();
+        }
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
---
+++ Controllers/PortalController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace portal.Controllers
+{
+    public class portalController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Appointments()
+        {
+            return View();
+        }
+        public IActionResult Patients()
+        {
+            return View();
+        }
+        public IActionResult Settings()
+        {
+            return View();
+        }
+    }
+}
